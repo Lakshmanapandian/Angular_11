580f116512098ce983065d8d63b8389030af2128
Lakshmana Pandian P <laks2988@global.chainsys.com>
2022-05-04 10:10:58 +0530

    included angular11

--- project-eleven/src/app/app.module.ts
+++ project-eleven/src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ReactiveFormsModule,FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmpformComponent } from './empform/empform.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmpformComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
